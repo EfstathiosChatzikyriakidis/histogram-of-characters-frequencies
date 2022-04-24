4b04a3cb6edc622a77296c9d6e91257b238ef7fe
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:34:34 +0300

    Change.

--- histogram-of-characters-frequencies.c
+++ histogram-of-characters-frequencies.c
@@ -1,7 +1,7 @@
 /*
  *  Prints a histogram of characters frequencies.
  *
- *  Copyright (C) 2007 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2007 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
